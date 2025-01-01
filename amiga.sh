28c427af5aaaec7b1e8c441fcb15c4960cfc3eef
Gareth Davidson <gaz@bitplane.net>
2025-01-01 09:26:37 +0000

    amiga

---
+++ amiga.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+clear
+
+# ANSI escape codes for formatting
+FLASH_RED="\033[5;31;40m"  # Flashing red text with black background
+RED="\033[25;1;31;40m"     # Bold red text with black background, explicitly disables flashing
+OFF="\033[0m"              # Reset formatting
+
+echo "$FLASH_RED▛▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▜$OFF"
+echo "$FLASH_RED▌  ${RED}Software Failure.  Press left mouse button to continue  $FLASH_RED▐$OFF"
+echo "$FLASH_RED▌            ${RED}Guru Meditation #00000004.0000AAC0            $FLASH_RED▐$OFF"
+echo "$FLASH_RED▙▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▟$OFF"
+echo "$RED                                                            $OFF"
+echo "$RED                                                            $OFF"
+echo "$RED                                                            $OFF"
+echo "$RED                                                            $OFF"
+echo "$RED                                                            $OFF"
+echo "$RED                                                            $OFF"
+echo "$RED                                                            $OFF"
+echo "$RED                                                            $OFF"
+echo "$RED                                                            $OFF"
+echo "$RED                                                            $OFF"
+echo "$RED                                                            $OFF"
+echo "$RED                                                            $OFF"
+echo "$RED                                                            $OFF"
+echo "$RED                                                            $OFF"
+echo "$RED                                                            $OFF"
