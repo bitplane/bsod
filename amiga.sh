eafe7d30c87eb55fb62685cedcf81cfe27695854
Gareth Davidson <gaz@bitplane.net>
2025-01-01 10:37:34 +0000

    wierd shit

--- amiga.sh
+++ amiga.sh
@@ -7,8 +7,8 @@ RED="\033[25;1;31;40m"     # Bold red text with black background, explicitly dis
 OFF="\033[0m"              # Reset formatting
 
 echo "$FLASH_RED▛▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▜$OFF"
-echo "$FLASH_RED▌  ${RED}Software Failure.  Press left mouse button to continue  $FLASH_RED▐$OFF"
-echo "$FLASH_RED▌            ${RED}Guru Meditation #00000004.0000AAC0            $FLASH_RED▐$OFF"
+echo "$FLASH_RED▌${RED}  Software Failure.  Press left mouse button to continue  $FLASH_RED▐$OFF"
+echo "$FLASH_RED▌${RED}            Guru Meditation #00000004.0000AAC0            $FLASH_RED▐$OFF"
 echo "$FLASH_RED▙▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▟$OFF"
 echo "$RED                                                            $OFF"
 echo "$RED                                                            $OFF"
